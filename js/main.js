eec705e9d464eddfa9ea0a3fe31bc4626bd98389
nomuna <genarek@gmail.com>
2021-09-08 19:26:07 +0200

    added rest of the code/resources to the cache

--- js/main.js
+++ js/main.js
@@ -13,6 +13,8 @@
  See the License for the specific language governing permissions and
  limitations under the License.
  */
+import swURL from 'sw:../service-worker.js';
+
 
 window.addEventListener('DOMContentLoaded', async () => {
   // Set up the editor
@@ -28,3 +30,17 @@ window.addEventListener('DOMContentLoaded', async () => {
 
   editor.setContent(defaultText);
 });
+
+// Register the service worker
+if ('serviceWorker' in navigator) {
+  // Wait for the 'load' event to not block other work
+  window.addEventListener('load', async () => {
+    // Try to register the service worker.
+    try {
+      const reg = await navigator.serviceWorker.register(swURL);
+      console.log('Service worker registered! 😎', reg);
+    } catch (err) {
+      console.log('😥 Service worker registration failed: ', err);
+    }
+  });
+}
